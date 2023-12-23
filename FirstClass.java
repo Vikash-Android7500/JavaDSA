8fabf3ceb6cb38ca9d0ddc9fb8203a889e6617a4
Vikash-Android7500 <Vikash75.jm@gmail.com>
2023-12-23 22:44:50 +0530

    java first day

---
+++ FirstClass.java
@@ -0,0 +1,15 @@
+class firstClass { //void main function khali hota hai.     
+     public static void main(String[] args) {
+          int number1 = 10;
+          int number2 = 20;
+          String Name = "vikash";
+          System.out.println(Name + "\n");
+          System.out.println(number1 + number2);
+
+     }
+}
+
+// (1) compilation
+// (2) Execution
+
+// (3) JDK ke under JRE ke under JVM our  Run code...
